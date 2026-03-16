1b182b0565964f178ce6c5261b1753e5ff576f35
MV Rajasree <rajasree050405@gmail.com>
2026-03-16 20:08:13 +0530

    Refactor isBalanced function and main input handling
    Refactored isBalanced function for clarity and updated main logic to improve readability.

--- main.cpp
+++ main.cpp
@@ -1,45 +1,45 @@
-#include <iostream>
-#include <stack>
-#include <string>
+#include <iostream> //input output
+#include <stack> //stack data structure
+#include <string> //text and string handling
 
 using namespace std;
 
 
-bool isBalanced(string exp){
-    if(exp.length() % 2 != 0) return false; //odd-length string cannot be balanced
+bool isBalanced(string expression){
+    if(expression.length() % 2 != 0) return false; //odd-length string cannot be balanced
     
-    stack<char> s;
-    for(char current:exp){
+    stack<char> pile; //LIFO structure of characters with the stack name 'pile'
+    for(char current:expression){ //modified for loop to reduce small mistakes
         if(current == '(' || current == '{' || current == '['){
-            s.push(current);
+            pile.push(current); //push onto the stack
         }
         else if(current == ')' || current == '}' || current == ']'){
-            if (s.empty()) return false;
+            if (pile.empty()) return false; //if we get the closing bracket but the stack is empty then we have no opening bracket to balance
             
-            char top = s.top();
+            char top = pile.top(); 
             if ((current == ')' && top == '(') ||
                 (current == '}' && top == '{') ||
                 (current == ']' && top == '[')) {
-                s.pop();
+                pile.pop(); // if closer matches the opener then pop 
         } else {
             return false;
         }
     }
 } 
-return s.empty();
+return pile.empty();
 }
 int main() {
-    int t;
-    if(!(cin >> t)) return 0;
+    int total;
+    if(!(cin >> total)) return 0; //taking number of inputs/strings from the user
     
-    while (t--) {
-        string input;
+    while (total--) {
+        string input; //input individual strings
         cin >> input;
         
         if (isBalanced(input))
-            cout << "YES" << endl;
+            cout << "Is Balanced" << endl;
         else
-            cout << "NO" << endl;
+            cout << "Is Not Balanced" << endl;
     }
     return 0;
 }
